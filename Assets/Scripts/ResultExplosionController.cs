54f9df0845e35307514c30a7be614bf2446f4ffd
yunishimura0716 <yunishimura0716@gmail.com>
2020-12-26 15:59:37 -0800

    added new background image

---
+++ Assets/Scripts/ResultExplosionController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultExplosionController : MonoBehaviour
+{
+    public GameObject explosionParticle;
+    [SerializeField]
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Explosion();
+    }
+    public void Explosion()
+    {
+        explosionParticle.SetActive(true);//爆発をBombの場所にBombの向きで呼び出す
+        StartCoroutine(destroyExplosionTimer());//Explosionを3秒後に消す
+    }
+
+    IEnumerator destroyExplosionTimer()
+    {
+        yield return new WaitForSeconds(3);
+        explosionParticle.SetActive(false);
+
+    }
+}
